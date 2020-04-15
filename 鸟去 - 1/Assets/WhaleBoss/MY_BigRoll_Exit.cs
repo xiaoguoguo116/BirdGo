ceb6d3a9c372697374c8efe315e6a21067937238
xiaoguoguo116 <52919328+xiaoguoguo116@users.noreply.github.com>
2020-04-15 21:16:05 +0800

    guo

---
+++ 鸟去 - 1/Assets/WhaleBoss/MY_BigRoll_Exit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MY_BigRoll_Exit : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject Coral;
+    [HideInInspector]
+    public bool bExit = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bExit)
+        {
+            Vector3 dst = new Vector3 (Coral.transform.position.x, Coral.transform.position.y, Camera.main.transform.position.z);
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, dst, Time.deltaTime);
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == Coral)  // 若乌贼到达出口，镜头改为跟随乌贼
+        {
+            bExit = true;
+            //Coral.GetComponent<CircleCollider2D>().enabled = false;
+            Camera.main.GetComponent<CameraFollow>().enabled = false;
+            //Invoke("Win", 5);
+        }
+    }
+
+    //void Win()
+    //{
+    //    Global.UI.GetComponent<UIManager>().victoryPanel.SetActive(true);
+    //    Global.Input.SetActive(false);
+    //    PlayerPrefs.SetInt("position", 0);
+    //    Camera.main.GetComponent<CameraFollow>().enabled = false;
+    //}
+}
---
+++ 鸟去 - 1/Assets/！团队LOGO/LOGO.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class LOGO : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] poster;
+
+    [SerializeField]
+    String nextScene;
+
+    int id = 0;
+    // Use this for initialization
+    void Start () {
+        foreach(var ob in poster)
+        {
+            ob.GetComponent<RawImage>().enabled = false;
+        }
+        StartCoroutine(ShowPaint());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //这里采用alpha渐变方法
+    IEnumerator ShowPaint()
+    {
+        RawImage image = poster[id].GetComponent<RawImage>();
+        image.enabled = true;
+        image.CrossFadeAlpha(0, 0f, true);//淡出执行参数，0=透明 0.5f=时间 true=
+        yield return new WaitForSeconds(0.7f);
+        image.CrossFadeAlpha(1, 1f, true);//淡入执行参数，1=显示 0.5f=时间 true=
+        yield return new WaitForSeconds(2f);
+        image.CrossFadeAlpha(0, 1f, true);//淡出执行参数，0=透明 0.5f=时间 true=
+        yield return new WaitForSeconds(0.7f);
+        if (++id < poster.Length)
+            StartCoroutine(ShowPaint());
+        else
+            SceneManager.LoadSceneAsync(nextScene);
+    }
+}

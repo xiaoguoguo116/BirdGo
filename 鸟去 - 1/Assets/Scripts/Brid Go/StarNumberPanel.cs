e379a60881ad10df53760f093433a700863054d5
xiaoguoguo116 <52919328+xiaoguoguo116@users.noreply.github.com>
2020-04-24 19:31:25 +0800

    prefabs

--- 鸟去 - 1/Assets/Scripts/Brid Go/StarNumberPanel.cs
+++ 鸟去 - 1/Assets/Scripts/Brid Go/StarNumberPanel.cs
@@ -5,14 +5,23 @@ using UnityEngine.UI;
 
 public class StarNumberPanel : MonoBehaviour {
 
-    public GameObject StarPosition1;
-    public GameObject star;
-    int number;
+    public Text StarText;     //得星星数目的显示UI
+    public int Num = 0;       //记录得星星数
     void Awake()
     {
-        number = GameObject.Find("GameManager").GetComponent<Start>().Num;
-       // if (number > 1)
-            Instantiate(star, Vector2.zero, Quaternion.identity);
+        Num = 0;
+        StarText.text = Num.ToString("0");  //开始获得星星数为0
     }
+    public void AddStart()    //星星数+1，并显示在UI上
+    {
+        Num++;
+        StarText.text = Num.ToString("0");//控制UI上星星数，并以整数形式显示
+        if (Num == 3)
+        {
+            //等视频播放完成（需加判断条件），
 
+            //显示面板
+ 
+        }
+    }
 }
---
+++ 鸟去 - 1/Assets/Scripts/Brid Go/star.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class star : MonoBehaviour {
+
+    GameObject UIPause;
+    void Start()
+    {
+        UIPause = GameObject.Find("UI Root");
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (UIPause.GetComponent<UIManager>().Pause)  //游戏在暂停状态时，星星碰撞后无效果
+             return;
+        else
+            Destroy(this.gameObject);
+    }
+}
